681901e483b0021d9977c7222117bf6294b32752
kimsiwooo <103231030+kimsiwooo@users.noreply.github.com>
2022-06-02 15:48:19 +0900

    Add files via upload

---
+++ import pybithumb.py
@@ -0,0 +1,46 @@
+import pybithumb
+import time
+import telegram
+from datetime import datetime
+
+all = pybithumb.get_current_price("ALL") 
+sort_all = sorted(all.items(), key = lambda x : float(x[1]['fluctate_rate_24H']), reverse=True)
+
+cycle_time = 1 
+loop_time = 30 * 1 
+ascent = 1.0 
+
+sec = 0
+prev_ticker = ''
+prev_rate = 0
+prev_dict = { 'ticker' : 0 }
+
+telgm_token = "5384687249:AAGCRGQnUqoRtMv7RPvVhzHR3vHtdaf1rW8" 
+            
+bot = telegram.Bot(token = telgm_token)
+bot.send_message(chat_id='5309877180', text="종목|현재가격|전체상승률|1초전상승률|상승률차이")
+
+print("   날짜    현재시간           종목   현재가격   전체상승률   1초전상승률   상승률차이")
+
+for ticker, data in sort_all :        
+    prev_dict[ticker] = data['fluctate_rate_24H']
+
+while sec < loop_time :
+    all = pybithumb.get_current_price("ALL") 
+    sort_all = sorted(all.items(), key = lambda x : float(x[1]['fluctate_rate_24H']), reverse=True)
+    
+    for ticker, data in sort_all :        
+        diff = float(data['fluctate_rate_24H']) - float(prev_dict[ticker])    
+        if diff >= ascent :
+            print(datetime.now(),"  ", ticker, "  ",data['closing_price'], "      ",data['fluctate_rate_24H'],"      ", float(prev_dict[ticker]),"      ", '%.2f' % diff )
+            a=ticker,data['closing_price'],data['fluctate_rate_24H'],float(prev_dict[ticker]), '%.2f' % diff
+            telgm_token = "5384687249:AAGCRGQnUqoRtMv7RPvVhzHR3vHtdaf1rW8"  
+            
+            bot = telegram.Bot(token = telgm_token)
+    
+            bot.send_message(chat_id='5309877180', text= a )
+
+        prev_dict[ticker] = data['fluctate_rate_24H']
+
+    time.sleep(cycle_time)
+    sec+=cycle_time
